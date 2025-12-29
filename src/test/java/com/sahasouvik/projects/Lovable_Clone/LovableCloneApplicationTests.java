25a96454330035cd8f0839eee7160ae952268d0e
Souvik Saha <souviksaha@Souviks-MacBook-Air.local>
2025-12-29 20:43:11 +0530

    Initial Setup

---
+++ src/test/java/com/sahasouvik/projects/Lovable_Clone/LovableCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahasouvik.projects.Lovable_Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovableCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
